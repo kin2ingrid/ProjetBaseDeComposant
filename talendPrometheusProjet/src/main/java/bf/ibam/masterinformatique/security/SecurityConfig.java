58b520b34d82d6440c715a0f5cf8021493beee83
Ingrid Kini <ingrid.kini@aikinggroup.tech>
2024-09-06 23:05:32 +0100

    DossierutilisationDeTalend

--- talendPrometheusProjet/src/main/java/bf/ibam/masterinformatique/security/SecurityConfig.java
+++ talendPrometheusProjet/src/main/java/bf/ibam/masterinformatique/security/SecurityConfig.java
@@ -1,5 +1,24 @@
 package bf.ibam.masterinformatique.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers("/actuator/**").permitAll() // Utilise requestMatchers au lieu de antMatchers
+                    .anyRequest().authenticated()
+            )
+            .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
 
 }
